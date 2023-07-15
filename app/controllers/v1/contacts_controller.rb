c1aa3d6b3e79336f9ee8c79934804175567168dd
Márcio Júnior <marcio.mj70@gmail.com>
2023-07-15 13:53:56 -0300

    feat: contacts controller

--- app/controllers/v1/contacts_controller.rb
+++ app/controllers/v1/contacts_controller.rb
@@ -2,7 +2,32 @@
 
 class V1::ContactsController < ApplicationController
   def index
-    @contacts = Contact.ActionController
+    @contacts = Contact.all
     render json: @contacts, status: :ok
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+
+    @contact.save
+    render json: @contact, status: :created
+  end
+
+  def destroy
+    @contact = Contact.where(id: params[:id]).first
+
+    return render json: { error: 'contact not found' }, status: :not_found if @contact.nil?
+
+    if @contact.destroy
+      head(:no_content)
+    else
+      head(:unprocessable_entity)
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:first_name, :last_name, :email)
   end
 end
