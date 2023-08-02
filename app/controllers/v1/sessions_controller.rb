89b62bd82988bb8971b01b1897c994c56758e3a1
Márcio Júnior <marcio.mj70@gmail.com>
2023-08-02 09:15:03 -0300

    chore: organize session controller

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -11,15 +11,9 @@ class V1::SessionsController < ApplicationController
     return render json: { error: 'user not found' }, status: :not_found if @user.nil?
 
     if @user.valid_password?(params[:password])
-      jwt = JWT.encode(
-        { user_id: @user.id, exp: (Time.now + 2.weeks).to_i },
-        Rails.application.secrets.secret_key_base,
-        'HS256'
-      )
-
       render :create, status: :created, locals: { token: jwt }
     else
-      head(:unauthorized)
+      render json: { error: 'invalid credentials' }, status: :unauthorized
     end
   end
 
@@ -30,5 +24,15 @@ class V1::SessionsController < ApplicationController
     else
       render json: { error: current_user.errors }, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def jwt
+    JWT.encode(
+      { user_id: @user.id, exp: (Time.now + 2.weeks).to_i },
+      Rails.application.secrets.secret_key_base,
+      'HS256'
+    )
   end
 end
